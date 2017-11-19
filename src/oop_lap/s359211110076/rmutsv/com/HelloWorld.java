32beff8cf3ecbe6d046906ad8b8ba7c636611acd
sofone <sofone.dekdee@gmail.com>
2017-11-19 14:49:14 +0700

    testing upload to github

---
+++ src/oop_lap/s359211110076/rmutsv/com/HelloWorld.java
@@ -0,0 +1,13 @@
+package oop_lap.s359211110076.rmutsv.com;
+
+public class HelloWorld {
+
+//  create main_method
+public static void main(String[] args) {
+
+    System.out.println("Hello World,I'am ONE");
+
+
+}// main
+
+}// class
