9272797c6155598d6a114d1de5166b7fc5634768
one <sofone.dekdee@gmail.com>
2017-12-06 09:58:14 +0700

    Testing while

---
+++ src/oop_lab3/TestDoWhileLoop.java
@@ -0,0 +1,22 @@
+package oop_lab3;
+
+import java.util.Scanner;
+
+public class TestDoWhileLoop {
+    public static void main(String[] args) {
+      // do-while
+      int n=1;
+      do {
+          //statement
+          System.out.print(n+" ");
+
+
+      }while (++n <=10);
+
+
+
+
+    }//main
+
+
+}//class
