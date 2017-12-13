e099e3703f96030ab293e60f80bf4af7e9866c67
one <sofone.dekdee@gmail.com>
2017-12-13 11:09:50 +0700

    Test Array List

--- src/oop_lab5/TestArrayList.java
+++ src/oop_lab5/TestArrayList.java
@@ -1,7 +1,35 @@
 package oop_lab5;
 
+import java.util.ArrayList;
+
 public class TestArrayList {
     public static void main(String[] args) {
+// ArrayList
+        ArrayList list = new ArrayList();
+        list.add("One");
+        list.add("BOY");
+        list.add("GIRL");
+        System.out.println(list);
+        System.out.println(list.size());
+        list.remove(1);
+        System.out.println(list);
+        list.add(1,"YaYa");
+        System.out.println(list);
+//   print with for loop
+        for (int i=0;i<list.size();i++){
+            System.out.println(list.get(i));
+
+        }//for i
+
+        System.out.println(list.indexOf("YAYA"));
+        list.set(1,"BABY");
+        System.out.println(list);
+
+
+
+
 
     }//main
+
+
 }//class
