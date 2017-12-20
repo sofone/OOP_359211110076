0e5e0c6257687c0a25cd8245a0f82489e2a09b95
LAB_COM-MT_1 <sofone.dekdee@gmail.com>
2017-12-20 11:16:26 +0700

    Test myString class

--- src/oop_lab5/TestsotingArray.java
+++ src/oop_lab5/TestsotingArray.java
@@ -1,4 +1,17 @@
 package oop_lab5;
 
 public class TestsotingArray {
-}
+    public static void main(String[] args) {
+        int number[] = {1, 2, 3, 4, 5,};
+            String msg[] = {"Cat","Dog","Bird"};
+            System.out.println(msg.length);
+            int number2[] = {1,2,3};
+
+
+
+
+
+    }//main
+
+
+}//class
