56f9b2c9a48c4b1ddfc5e72e78c1fcd331617a8a
LAB_COM-MT_1 <sofone.dekdee@gmail.com>
2017-12-20 11:51:53 +0700

    Test myString Token

--- src/oop_lap6/myStringToken.java
+++ src/oop_lap6/myStringToken.java
@@ -1,4 +1,21 @@
 package oop_lap6;
+import java.util.StringTokenizer;
 
 public class myStringToken {
-}
+    public static void main(String[] args) {
+        String S = "Welcome to RMUSTV";
+        StringTokenizer tokenizer = new StringTokenizer(S);
+        // count word in String
+
+        System.out.println(tokenizer.countTokens());
+        // cut word in String
+        // reverse word String
+        while (tokenizer.hasMoreTokens()){
+            StringBuffer buffer = new StringBuffer(tokenizer.nextToken());
+            System.out.println(buffer.reverse());
+        }
+
+
+
+    }//main
+}//class
